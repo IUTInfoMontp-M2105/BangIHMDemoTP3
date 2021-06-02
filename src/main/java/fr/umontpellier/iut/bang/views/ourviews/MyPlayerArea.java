98ba52e1f94b8ea6fa3c3359b0e6a3a5d86ed603
Sophie NABITZ <sophie.nabitz@univ-avignon.fr>
2021-06-02 12:56:07 +0200

    Ajout de la main du joueur dans la zone d'un joueur

--- src/main/java/fr/umontpellier/iut/bang/views/ourviews/MyPlayerArea.java
+++ src/main/java/fr/umontpellier/iut/bang/views/ourviews/MyPlayerArea.java
@@ -1,16 +1,38 @@
 package fr.umontpellier.iut.bang.views.ourviews;
 
+import fr.umontpellier.iut.bang.ICard;
 import fr.umontpellier.iut.bang.IPlayer;
+import fr.umontpellier.iut.bang.logic.cards.*;
 import fr.umontpellier.iut.bang.views.GameView;
 import fr.umontpellier.iut.bang.views.PlayerArea;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static fr.umontpellier.iut.bang.logic.cards.CardSuit.*;
 
 public class MyPlayerArea extends PlayerArea {
 
+    private VBox vueJoueur;
+    private HBox laMainDuJoueur;
+
     public MyPlayerArea(IPlayer player, GameView gameView) {
         super(player, gameView);
+        vueJoueur = new VBox();
         Label nomJoueur = new Label(player.getName());
-        getChildren().add(nomJoueur);
+        laMainDuJoueur = new HBox();
+        List<Card> cartes = new ArrayList<>();
+        cartes.add(new Beer(6, CardSuit.HEART));
+        cartes.add(new Bang(14, SPADE));
+        cartes.add(new CatBalou(9, DIAMOND));
+        for (Card c : cartes)
+            laMainDuJoueur.getChildren().add(new MyCardView(new ICard(c), this));
+        vueJoueur.getChildren().add(nomJoueur);
+        vueJoueur.getChildren().add(laMainDuJoueur);
+        getChildren().add(vueJoueur);
     }
 
     @Override
