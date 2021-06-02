b5d1de2e01f6a6b38e82f072599c093672877641
Sophie NABITZ <sophie.nabitz@univ-avignon.fr>
2021-06-02 12:42:15 +0200

    Ajout des joueurs dans la vue principale

--- src/main/java/fr/umontpellier/iut/bang/views/ourviews/MyGameView.java
+++ src/main/java/fr/umontpellier/iut/bang/views/ourviews/MyGameView.java
@@ -1,12 +1,20 @@
 package fr.umontpellier.iut.bang.views.ourviews;
 
 import fr.umontpellier.iut.bang.IGame;
+import fr.umontpellier.iut.bang.IPlayer;
+import fr.umontpellier.iut.bang.logic.Player;
 import fr.umontpellier.iut.bang.views.GameView;
+import javafx.scene.layout.VBox;
 
 public class MyGameView extends GameView {
 
+    private VBox vuePrincipale;
     public MyGameView(IGame game) {
         super(game);
+        vuePrincipale = new VBox();
+        for (Player p : game.getPlayers())
+            vuePrincipale.getChildren().add(new MyPlayerArea(new IPlayer(p), this));
+        getChildren().add(vuePrincipale);
     }
 
     @Override
